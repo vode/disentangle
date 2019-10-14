37ea2b24773703f62caadea1e69bc28129b8289c
vode <kylayamata@hotmail.com>
2019-10-14 23:38:59 +0800

    add train script

---
+++ train.sh
@@ -0,0 +1,1 @@
+python3 disentangle.py example-train --train ./data/train/*annotation.txt --dev ./data/dev/*annotation.txt --hidden 256 --word-vectors glove-ubuntu.txt --epochs 20  --learning-rate 0.0018804 --learning-decay-rate 0.103 --seed 10 --clip 3.740 --weight-decay 1e-07 
